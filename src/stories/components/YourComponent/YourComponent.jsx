5689f85aaa60ac3c5872e2811f24b2cc6960119a
daniel <danielgarridolizana@gmail.com>
2022-01-29 11:47:48 +0100

    first component

---
+++ src/stories/components/YourComponent/YourComponent.jsx
@@ -0,0 +1,25 @@
+import React,{useState} from 'react';
+
+
+export const YourComponent = (props) => {
+    const {primary} = props;
+    const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+    const [name,setName] = useState('');
+    const handleClick= () => {
+        setName('Hola soy Daniel')
+        if(name !=='') {
+            setName('')
+        }
+    }
+    return (
+        <>
+            <button
+                className= {mode}
+                onClick={handleClick}
+            >
+                Hola
+            </button>
+            <p>{name}</p>
+        </>
+    )
+}
