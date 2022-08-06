f6046915f4549d8310f9d442f0afa9e651697708
danugarri <danugarri@hotmail.com>
2022-08-06 10:48:39 +0200

    try to publish in npm 1

--- packages/index.js
+++
@@ -1,5 +0,0 @@
-import { BotonDaniel } from "./BotonDaniel/BotonDaniel";
-import { Spinner } from "./Spinner/Spinner";
-import { SideBar } from "./SideBar/SideBar";
-
-export {BotonDaniel,Spinner,SideBar}
